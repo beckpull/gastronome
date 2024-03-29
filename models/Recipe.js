1ce1335b86fc912f5554f415c43b2ea3633501c0
Rebecca Feltman <153051811+refeltman83@users.noreply.github.com>
2024-03-28 21:07:45 -0600

    added initial file structure and models

---
+++ models/Recipe.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Recipe extends Model {}
+
+Recipe.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    recipe_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    filename: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    ingredients: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    instructions: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    modelName: "comment",
+  }
+);
+
+module.exports = Recipe;
