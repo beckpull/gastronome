8996996a0109a5d0c2e1ed7f3e772ddb6fe55539
Rebecca Feltman <153051811+refeltman83@users.noreply.github.com>
2024-04-04 15:44:44 -0600

    fixed variable naming convention in addComment.js in public dir

--- public/js/addComment.js
+++ public/js/addComment.js
@@ -1,12 +1,12 @@
 const newPostFormHandler = async (event) => {
     event.preventDefault();
-    const postId = window.location.pathname.split('/').pop(); 
-    const contents = document.querySelector("#comment-contents").value.trim();
-    console.log(postId);
-    console.log(contents);
+    const recipeId = window.location.pathname.split('/').pop(); 
+    const contents = document.querySelector("#comment-contents").value.trim(); // Content inpute must either be labeled with id="comment-contents" or we will need to change this
+    // console.log(recipeId);
+    // console.log(contents);
     if (contents) {
  
-      const response = await fetch(`/api/comments/${postId}`, {
+      const response = await fetch(`/api/comments/${recipeId}`, {
         method: "POST",
         body: JSON.stringify({ contents }),
         headers: { "Content-Type": "application/json" },
@@ -20,5 +20,5 @@ const newPostFormHandler = async (event) => {
   };
 
 document
-  .querySelector(".addcomment-card")
+  .querySelector(".newcomment-form") // Form will either need to have class="newcomment-form" or we will need to change this
   .addEventListener("submit", newPostFormHandler);
