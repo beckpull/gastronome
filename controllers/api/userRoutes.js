9f55305d35fa58130a98ad0f6948156b9a5f8dfb
Rebecca Feltman <153051811+refeltman83@users.noreply.github.com>
2024-04-12 11:26:58 -0600

    trying to fix bug

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -73,5 +73,16 @@ router.post('/logout', (req, res) => {
     }
 });
 
+router.get('/logout', (req, res) => {
+    // console.log("Hi!!!!!");
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.redirect('/');
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
 
 module.exports = router;
